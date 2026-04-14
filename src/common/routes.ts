d5409e3fb1c2e066eaaa1166c05cc0da02de7fdc
Mintori09 <tvmthtr@gmail.com>
2026-04-14 16:44:11 +0700

    feat(routes): register item donation and item phase routes

--- src/common/routes.ts
+++ src/common/routes.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express'
 import { authRouter } from 'src/features/auth'
 import { passwordRouter } from 'src/features/forgotPassword'
+import { itemPhaseRouter } from 'src/features/item-phase'
+import { itemDonationRouter } from 'src/features/item-donation'
 import { authLimiter } from 'src/common/middleware'
 import { config } from 'src/config'
 
@@ -18,12 +20,38 @@ const defaultRoutes = [
     },
 ]
 
+const itemPhaseRoutes = [
+    {
+        path: '/campaigns/:campaignId/item-phases',
+        route: itemPhaseRouter,
+    },
+]
+
+const itemDonationRoutes = [
+    {
+        path: '/donations',
+        route: itemDonationRouter,
+    },
+    {
+        path: '/item-phases',
+        route: itemDonationRouter,
+    },
+]
+
 defaultRoutes.forEach((route) => {
     if (route.limiter) {
         router.use(route.path, route.limiter, route.route)
     } else {
         router.use(route.path, route.route)
     }
+})
+
+itemPhaseRoutes.forEach((route) => {
+    router.use(route.path, route.route)
+})
+
+itemDonationRoutes.forEach((route) => {
+    router.use(route.path, route.route)
 })
 
 export default router
